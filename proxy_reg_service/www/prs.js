9abc05a74dc5b03fb0e01657a899f32fe8a81020
Andy <Andy@shortroundgames.com>
2018-03-02 14:45:12 +0000

    each ip is now assigned an integer id

--- proxy_reg_service/www/prs.js
+++ proxy_reg_service/www/prs.js
@@ -5,6 +5,7 @@ process.chdir(__dirname);
 
 var init = require('./init/init');
 var redisObjects = rootRequire('./sr_lib/redisObjects');
+var redisLock = rootRequire('./sr_lib/redis/redisLock');
 var ip = require('ip');
 
 async.series(
@@ -41,28 +42,83 @@ async.series(
                     callback(err);
                 });
         }
-                
-        console.log("Starting Server");
 
-        var app = server.startServer(
-            null,
-            null,
-            health,
-            nconf.get('server'));
-        
         var redis = redisClientPool.getClient("proxy_reg");
-        var heartbeat_set = new redisObjects.SortedSet("proxy_reg:heatbeat", redis, {marshal:true});        
-        
-        var url = ip.address()
+        var id_hash = new redisObjects.Hash("proxy_reg:ids", redis, { marshal: true });
 
-        heartbeat_set.set(url, Date.now(), srlog.errorCallback);
+        var url = ip.address();
+        var url_id = -1;
 
-        setInterval(
-            function()
+        async.series(
+            [
+                //Check if we already have an address
+                function(next)
+                {                    
+                    id_hash.get(url,
+                        function(err, id)
+                        {
+                            if (err)
+                                return next(err);
+
+                            if (id)
+                            {
+                                url_id = id;
+                                return next("SUCCESS");
+                            }
+
+                            next();
+                        });
+                },
+                //We need to create an id for this url
+                function(next)
+                {
+                    redisLock.lockedFunction(redis, "proxy_reg:idlock", 5, 120,
+                        function(next)
+                        {
+                            id_hash.length(
+                                function(err, num)
+                                {
+                                    if (err)
+                                        return next(err);
+
+                                    url_id = num+1;
+                                    id_hash.set(url, url_id, next);
+                                });
+                        },
+                        next);
+                }
+            ],
+            function(err)
             {
-                heartbeat_set.set(url, Date.now(), srlog.errorCallback);
-            },
-            60 * 1000);
+                if (err && err != "SUCCESS")
+                {
+                    srlog.error("Failed to find/create id");
+                    exit(1);
+                    return;
+                }
+
+                console.log("Starting Server");
+
+                var app = server.startServer(
+                    null,
+                    null,
+                    health,
+                    nconf.get('server'));
+
+
+                var heartbeat_set = new redisObjects.SortedSet("proxy_reg:heatbeat", redis, { marshal: true });
+                var data = url_id + '#' + url;
+
+                heartbeat_set.set(data, Date.now(), srlog.errorCallback);
+
+                setInterval(
+                    function()
+                    {
+                        heartbeat_set.set(data, Date.now(), srlog.errorCallback);
+                    },
+                    60 * 1000);
+
+            });
     });
 
 
